29340d27ea7e020e37c445f1046d709c6b10beaf
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 18:11:33 -0500

    initialize create update destroy show;

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,9 +1,49 @@
 class Api::ReviewsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :create, :update, :destroy, :show]
+  before_action :set_review, only: [:show, :update, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
-    # book = Book.find(params[:book_id])
     @reviews = Review.where(book_id: params[:book_id])
     render :index
+  end
+
+  def show
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # @review.user = current_user
+    if @review.save
+      render :show
+    else
+      render json: @review.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @review.update(review_params)
+      render :show
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    if @review.destroy
+      render :show
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment, :suggest, :book_id, :user_id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 end
