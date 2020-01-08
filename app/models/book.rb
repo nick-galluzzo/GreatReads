4954ee69816f6a032727e192291097800dce89be
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 16:04:23 -0500

    remove full_name class method

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,8 +11,4 @@ class Book < ApplicationRecord
   validates :description, presence: true
   validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }
   validates :image, presence: true
-
-  def self.full_name(first_name, last_name)
-    first_name + '' + last_name
-  end
 end
