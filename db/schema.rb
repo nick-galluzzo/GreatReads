ae82de85cd79b071ba8f94bf9d9c78886ed219a4
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 13:07:34 -0500

    add bookshelves, book_subscriptions, and reviews to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_172838) do
+ActiveRecord::Schema.define(version: 2020_01_08_180523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_01_08_172838) do
     t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "book_subscriptions", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "bookshelf_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_subscriptions_on_book_id"
+    t.index ["bookshelf_id"], name: "index_book_subscriptions_on_bookshelf_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -35,10 +44,30 @@ ActiveRecord::Schema.define(version: 2020_01_08_172838) do
     t.index ["genre_id"], name: "index_books_on_genre_id"
   end
 
+  create_table "bookshelves", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookshelves_on_user_id"
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating", null: false
+    t.boolean "suggest"
+    t.bigint "user_id"
+    t.bigint "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,6 +83,11 @@ ActiveRecord::Schema.define(version: 2020_01_08_172838) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "book_subscriptions", "books"
+  add_foreign_key "book_subscriptions", "bookshelves"
   add_foreign_key "books", "authors"
   add_foreign_key "books", "genres"
+  add_foreign_key "bookshelves", "users"
+  add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
