5cfb9694428ddce98597086291ce71dc19b0be54
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 18:13:00 -0500

    code cleanup and validation of uniqueness of user id on book id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,10 @@
 class Review < ApplicationRecord
+  validates :rating,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: (1..5).to_a }
+  validates_uniqueness_of :user_id, scope: [:book_id]
+
   belongs_to :user
   belongs_to :book
-
-  validates :rating, presence: true, numericality: { only_integer: true }
 end
