69023ad5b2cb37fc557ef1b1e6d78c9522e8ef5e
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 15:40:11 -0500

    add class method full_name to book class

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,4 +11,8 @@ class Book < ApplicationRecord
   validates :description, presence: true
   validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }
   validates :image, presence: true
+
+  def self.full_name(first_name, last_name)
+    first_name + '' + last_name
+  end
 end
