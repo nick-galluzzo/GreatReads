d10940dda8f22f002778e51612407ef0af4d9d86
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 18:11:59 -0500

    code cleanup

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ApplicationRecord
+  validates :name, presence: true
+
   has_many :books, dependent: :destroy
   has_many :genres,
            through: :books
-
-  validates :name, presence: true
 end
