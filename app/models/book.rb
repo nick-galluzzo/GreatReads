1a3bc760e84613fa3e0c7ca68aa15b2f806456a3
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 13:00:55 -0500

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :image, presence: true
 end
