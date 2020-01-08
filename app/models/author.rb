a0ab93dacc7d7a65d2951725e830b0bed7395265
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 14:06:33 -0500

    add associations and validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,7 @@ class Author < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :genres,
            through: :books
+
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
