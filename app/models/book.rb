eef82e6d49056132915d3d10cc1411a7bdda80a7
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 14:06:58 -0500

    add associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,11 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :genre
 
+  has_many :reviews, dependent: :destroy
+  has_many :book_subscriptions, dependent: :destroy
+  has_many :bookshelves,
+           through: :book_subscriptions
+
   validates :title, presence: true
   validates :description, presence: true
   validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }
