7776cbbbf65097ed24cdf557b450754dba4eca90
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 14:07:17 -0500

    add associations and validations

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -1,3 +1,9 @@
 class Bookshelf < ApplicationRecord
   belongs_to :user
+
+  has_many :book_subscriptions, dependent: :destroy
+  has_many :books,
+           through: :book_subscriptions
+
+  validates :name, presence: true, acceptance: { accept: ['Want to read', 'Currently Reading', 'Read'] }
 end
