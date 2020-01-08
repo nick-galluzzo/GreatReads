99f51e3913d55c4ede34869ac745d0bbaee7d59c
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 18:19:11 -0500

    add users and bookshelves routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   namespace :api, defaults: { format: :json } do
+    resources :users, only: [ :show, :create, :destroy ] do
+      resources :bookshelves, only: [ :index ]
+    end
     resources :books, only: [ :index, :show ] do
       resources :reviews, only: :index
     end
     resources :authors, only: :show
+    resources :bookshelves, only: [:show]
     resources :genres, only: :show
     resources :reviews, only: [:create, :show, :update, :destroy]
   end
