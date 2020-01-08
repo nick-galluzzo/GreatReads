3eb882ad401aa3dbca80b271774db9bf54e745e8
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 13:00:41 -0500

    add presence validations to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :genres,
            through: :books
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
