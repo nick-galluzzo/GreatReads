cc90db52aa8c28b746fd2ebf125ac323683a7479
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 18:12:13 -0500

    code cleanup

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :image, presence: true
+
   belongs_to :author
   belongs_to :genre
 
@@ -6,9 +11,4 @@ class Book < ApplicationRecord
   has_many :book_subscriptions, dependent: :destroy
   has_many :bookshelves,
            through: :book_subscriptions
-
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :isbn, presence: true, uniqueness: true, numericality: { only_integer: true }
-  validates :image, presence: true
 end
