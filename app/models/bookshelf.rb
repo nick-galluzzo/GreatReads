99283f6fbc9d5d0577f06f803df616b8bc1b2c78
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 18:12:32 -0500

    code cleanup

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -1,9 +1,9 @@
 class Bookshelf < ApplicationRecord
+  validates :name, presence: true, inclusion: { in: ['Want to read', 'Currently Reading', 'Read'] }
+
   belongs_to :user
 
   has_many :book_subscriptions, dependent: :destroy
   has_many :books,
            through: :book_subscriptions
-
-  validates :name, presence: true, acceptance: { accept: ['Want to read', 'Currently Reading', 'Read'] }
 end
