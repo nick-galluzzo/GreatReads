15e267c4e39687931f65a9dc892e4381df03cb29
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 18:29:52 -0500

    added fetchAuthor action

--- app/javascript/app/actions/index.js
+++ app/javascript/app/actions/index.js
@@ -1,8 +1,7 @@
+// BOOK ACTIONS
+
 export const FETCH_BOOKS = 'FETCH_BOOKS';
 export const FETCH_BOOK = 'FETCH_BOOK';
-
-// temporary ROOT URL
-const ROOT_URL = 'localhost:3000';
 
 export const fetchBooks = () => {
   const promise = fetch('/api/books')
@@ -20,6 +19,21 @@ export const fetchBook = (id) => {
 
   return {
     type: FETCH_BOOK,
+    payload: promise,
+  };
+};
+
+// AUTHOR ACTIONS
+
+// export const FETCH_AUTHORS = 'FETCH_AUTHORS';
+export const FETCH_AUTHOR = 'FETCH_AUTHOR';
+
+export const fetchAuthor = (id) => {
+  const promise = fetch(`/api/authors/${id}`)
+    .then((response) => response.json());
+
+  return {
+    type: FETCH_AUTHOR,
     payload: promise,
   };
 };
