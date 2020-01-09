c787659e0d1c3cd573265899e86c998d1332c3a7
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 09:26:20 -0500

    import rootReducer

--- app/javascript/app/components/store.js
+++ app/javascript/app/components/store.js
@@ -1,7 +1,7 @@
 import { logger } from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxPromise from 'redux-promise';
-// import rootReducer from '../reducers/rootReducer';
+import rootReducer from '../reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
