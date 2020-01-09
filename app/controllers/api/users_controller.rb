495fa627892d0ea9057b400d2dcfef00cb3210f6
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 20:16:12 -0500

    initialize users show

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
+  def show
+    @user = User.includes(:bookshelves, :books).find(params[:id])
+    if @user
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
