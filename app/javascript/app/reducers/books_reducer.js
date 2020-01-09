58ca8d41a30eda7ddb8de5076cccd239529c8b1d
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 09:26:57 -0500

    add fetch books case

--- app/javascript/app/reducers/books_reducer.js
+++ app/javascript/app/reducers/books_reducer.js
@@ -1,1 +1,12 @@
-books_reducer.js
+import { FETCH_BOOKS } from '../actions/index';
+
+const booksReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_BOOKS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default booksReducer;
