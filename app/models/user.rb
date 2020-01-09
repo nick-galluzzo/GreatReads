21a0db4bf921421fdb433eb4224eea1d15709447
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 20:18:09 -0500

    user has many books through bookshelves and initialize init_shelves class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :init_shelves
+
+  attr_accessor :read_shelf, :will_read_shelf, :reading_shelf
+
   has_many :reviews, dependent: :destroy
   has_many :bookshelves, dependent: :destroy
-  has_many :book_subscriptions,
+
+  has_many :books,
            through: :bookshelves
+
+  private
+
+  def init_shelves
+    @read_shelf = Bookshelf.create(user_id: self.id, name: 'Read')
+    @will_read_shelf = Bookshelf.create(user_id: self.id, name: 'Want to Read')
+    @reading_shelf = Bookshelf.create(user_id: self.id, name: 'Currently Reading')
+  end
 end
