0abe8d670946bb066756abc9d150dcfaf3dbbd43
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-08 20:15:46 -0500

    initiailze index and show

--- app/controllers/api/bookshelves_controller.rb
+++ app/controllers/api/bookshelves_controller.rb
@@ -1,2 +1,13 @@
 class Api::BookshelvesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @bookshelves = Bookshelf.where(user_id: params[:user_id])
+    render :index
+  end
+
+  def show
+    @bookshelf = Bookshelf.includes(:books).find(params[:id])
+    render :show
+  end
 end
