a8a95619ce0f055d3c9cc57d1133437fc1e0db7f
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 16:06:49 -0500

    add class methods similar_authors and most_written_genre

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,34 @@
 class Author < ApplicationRecord
   validates :name, presence: true
 
+  # attr_accessor :authors
+
   has_many :books, dependent: :destroy
   has_many :genres,
            through: :books
+
+  def similar_authors
+    popular_genre = most_written_genre
+
+    popular_genre.authors.map do |author|
+      return author unless author == self
+    end
+  end
+
+  def most_written_genre
+    genres_count = calculate_genres
+    genres_count.key(genres_count.values.max)
+  end
+
+  private
+
+  def calculate_genres
+    @genres_count = Hash.new(0)
+
+    self.genres.each do |genre|
+      @genres_count[genre] += 1
+    end
+
+    @genres_count
+  end
 end
