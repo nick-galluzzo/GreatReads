e2859fda4c4f2b57537e1014f0e9d9fe6d03a995
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 09:25:07 -0500

    initialize

---
+++ app/javascript/app/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from '../containers/book_list';
+
+const App = (props) => {
+  return (
+    <div className="app">
+      <BookList />
+    </div>
+  );
+};
+
+export default App;
