b242d06e45e8d1b12a1462376dae61b8e9d44e05
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 18:35:29 -0500

    add authorsReducer

--- app/javascript/app/reducers/rootReducer.js
+++ app/javascript/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import booksReducer from './books_reducer';
+import authorsReducer from './authors_reducer';
 
 const rootReducer = combineReducers({
-  books: booksReducer
+  books: booksReducer,
+  authors: authorsReducer,
 });
 
 export default rootReducer;
