379f3dc8384980b9c43be59616101f77ab1a1bc1
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-09 21:30:08 -0500

    add review count and suggestion percentage functions

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
@@ -13,10 +14,27 @@ class Book < ApplicationRecord
            through: :book_subscriptions
 
   def average_rating
-    (ratings.reduce(:+) / ((ratings.count * 100.0).round / 100.0)).round(2)
+    ratings.count < 1 ? 'none' : (ratings.reduce(:+) / ((ratings.count * 100.0).round / 100.0)).round(2)
+  end
+
+  def review_count
+    reviews.count
+  end
+
+  def suggest_percentage
+    all_suggestions_count < 1 ? 'none' : ((suggest_true_count.to_f / all_suggestions_count.to_f) * 100).round
   end
 
   private
+
+  def all_suggestions_count
+    reviews.map(&:suggest).count
+  end
+
+  def suggest_true_count
+    suggestions = reviews.map(&:suggest)
+    suggestions.select {|s| s == true}.count
+  end
 
   def ratings
     self.reviews.map(&:rating)
