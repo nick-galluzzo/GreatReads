19b12aafb7c0ec11a5970b2276d6966d08ee9f35
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-10 19:12:26 -0500

    create more books

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,6 +44,46 @@ Book.create(
     isbn: '0670026190',
     image: 'https://prodimage.images-bn.com/pimages/9780143110439_p0_v1_s550x406.jpg'
   )
+Book.create(
+    title: "Rules of Civility",
+    author: Author.second,
+    genre: Genre.first,
+    description: 'On the last night of 1937, twenty-five-year-old Katey Kontent is in a second-rate Greenwich Village jazz bar with her boardinghouse roommate stretching three dollars as far as it will go when Tinker Grey, a handsome banker with royal blue eyes and a tempered smile, happens to sit at the neighboring table. This chance encounter and its startling consequences propel Katey on a yearlong journey from a Wall Street secretarial pool toward the upper echelons of New York society and the executive suites of Condé Nast--rarefied environs where she will have little to rely upon other than a bracing wit and her own brand of cool nerve.',
+    isbn: '0670022691',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/91fWfmE1GzL.jpg'
+  )
+Book.create(
+    title: "You Have Arrived at Your Destination",
+    author: Author.second,
+    genre: Genre.first,
+    description: 'When Sam’s wife first tells him about Vitek, a twenty-first-century fertility lab, he sees it as the natural next step in trying to help their future child get a “leg up” in a competitive world. But the more Sam considers the lives that his child could lead, the more he begins to question his own relationships and the choices he has made in his life.',
+    isbn: '9781732',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51JQAqbonaL.jpg'
+  )
+Book.create(
+    title: "Eve in Hollywood",
+    author: Author.second,
+    genre: Genre.first,
+    description: 'In this chain of six richly detailed and atmospheric stories, each told from a different perspective, Towles unfolds the events that take Eve to the heart of Old Hollywood. Beginning in the dining car of the Golden State Limited in September 1938, we follow Eve to the elegant rooms of the Beverly Hills Hotel, the fabled tables of Antonio’s, the amusement parks on the Santa Monica piers, the afro-Cuban dance clubs of Central Avenue, and ultimately the set of Gone with The Wind.',
+    isbn: '9781101',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1369321477l/17972257.jpg'
+  )
+Book.create(
+    title: "Narcissus and Goldmund",
+    author: Author.first,
+    genre: Genre.first,
+    description: "Narcissus and Goldmund tells the story of two medieval men whose characters are diametrically opposite: Narcissus, an ascetic monk firm in his religious commitment, and Goldmund, a romantic youth hungry for knowledge and worldly experience. First published in 1930, Hesse's novel remains a moving and pointed exploration of the conflict between the life of the spirit and the life of the flesh. It is a theme that transcends all time.",
+    isbn: '9721301',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81wBIEW0APL.jpg'
+  )
+Book.create(
+    title: "A Whimsy of the World",
+    author: Author.last,
+    genre: Genre.first,
+    description: "For as long as the town of Aurora could remember, Ellie Watson had been a spirited sort. On her way to the general store to pick up some safety pins for her mother, Ellie could easily take a left instead of a right, and end up on the banks of the Tanner River throwing stones at the silvery shadows of trout. And while getting dressed for the Sunday social, she could suddenly put on her brother’s overalls instead of her new yellow dress – despite her brother being four years older and two inches taller.",
+    isbn: '2922301',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1558396718l/41952927.jpg'
+  )
 
 # BookSubscription
 BookSubscription.create!(book_id: Book.first.id, bookshelf_id: User.first.bookshelves.last.id)
