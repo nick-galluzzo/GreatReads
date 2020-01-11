f521f9a117c2ca29524906a90c8a41cd39e81a52
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-11 18:17:09 -0500

    seed more authors and books

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,16 @@ User.create(email: 'test@gmail.com', password: 'test123', username: 'testtest')
 User.create(email: 'test1@gmail.com', password: 'test222', username: 'AbrahamLincoln')
 
 # Author
-Author.create(name: 'Herman Hesse')
-Author.create(name: 'Amor Towles')
+Author.create(name: 'Herman Hesse', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+Author.create(name: 'Amor Towles', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+
+orwell = Author.create(name: 'George Orwell', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+carlos = Author.create(name: 'Carlos Ruiz Zafón', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+bukowski = Author.create(name: 'Charles Bukowski', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+wong = Author.create(name: 'Jan Wong', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+frankyl = Author.create(name: 'Viktor E. Frankyl', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+wilde = Author.create(name: 'Oscar Wilde', image: 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png')
+
 
 # Genre
 Genre.create(name: 'fiction')
@@ -84,6 +92,80 @@ Book.create(
     isbn: '2922301',
     image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1558396718l/41952927.jpg'
   )
+
+Book.create(
+    title: "1984",
+    author: orwell,
+    genre: Genre.first,
+    description: "Among the seminal texts of the 20th century, Nineteen Eighty-Four is a rare work that grows more haunting as its futuristic purgatory becomes more real. Published in 1949, the book offers political satirist George Orwell's nightmarish vision of a totalitarian, bureaucratic world and one poor stiff's attempt to find individuality. The brilliance of the novel is Orwell's prescience of modern life—the ubiquity of television, the distortion of the language—and his ability to construct such a thorough version of hell.",
+    isbn: '2925701',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/410ZirPKXKL._SX331_BO1,204,203,200_.jpg'
+  )
+Book.create(
+    title: "Animal Farm",
+    author: orwell,
+    genre: Genre.first,
+    description: "A farm is taken over by its overworked, mistreated animals. With flaming idealism and stirring slogans, they set out to create a paradise of progress, justice, and equality. Thus the stage is set for one of the most telling satiric fables ever penned –a razor-edged fairy tale for grown-ups that records the evolution from revolution against tyranny to a totalitarianism just as terrible.",
+    isbn: '2912301',
+    image: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1410/9780141036137.jpg'
+  )
+Book.create(
+    title: "Shadow of the Wind",
+    author: carlos,
+    genre: Genre.first,
+    description: "Barcelona, 1945. Just after the war, a great world city lies in shadow, nursing its wounds, and a boy named Daniel awakes on his eleventh birthday to find that he can no longer remember his mother’s face. To console his only child, Daniel’s widowed father, an antiquarian book dealer, initiates him into the secret of the Cemetery of Forgotten Books, a library tended by Barcelona’s guild of rare-book dealers as a repository for books forgotten by the world, waiting for someone who will care about them again. Daniel’s father coaxes him to choose a volume from the spiraling labyrinth of shelves, one that, it is said, will have a special meaning for him.",
+    isbn: '2922261',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51kS6aEjFIL._SX324_BO1,204,203,200_.jpg'
+  )
+Book.create(
+    title: "Ham on Rye",
+    author: bukowski,
+    genre: Genre.first,
+    description: "In what is widely hailed as the best of his many novels, Charles Bukowski details the long, lonely years of his own hardscrabble youth in the raw voice of alter ego Henry Chinaski. From a harrowingly cheerless childhood in Germany through acne-riddled high school years and his adolescent discoveries of alcohol, women, and the Los Angeles Public Library's collection of D. H. Lawrence, 'Ham on Rye' offers a crude, brutal, and savagely funny portrait of an outcast's coming-of-age during the desperate days of the Great Depression.",
+    isbn: '1222301',
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388195001l/38501.jpg'
+  )
+Book.create(
+    title: "Post Office",
+    author: bukowski,
+    genre: Genre.first,
+    description: "'It began as a mistake.' By middle age, Henry Chinaski has lost more than twelve years of his life to the U.S. Postal Service. In a world where his three true, bitter pleasures are women, booze, and racetrack betting, he somehow drags his hangover out of bed every dawn to lug waterlogged mailbags up mud-soaked mountains, outsmart vicious guard dogs, and pray to survive the day-to-day trials of sadistic bosses and certifiable coworkers.",
+    isbn: '2322301',
+    image: 'https://www.sweetbooks.com/pictures/medium/SKB-12098.jpg'
+  )
+Book.create(
+    title: "Red China Blues: My Long March From Mao to Now",
+    author: wong,
+    genre: Genre.first,
+    description: "Red China Blues is Wong's startling--and ironic--memoir of her rocky six-year romance with Maoism (which crumbled as she became aware of the harsh realities of Chinese communism); her dramatic firsthand account of the devastating Tiananmen Square uprising; and her engaging portrait of the individuals and events she covered as a correspondent in China during the tumultuous era of capitalist reform under Deng Xiaoping. In a frank, captivating, deeply personal narrative she relates the horrors that led to her disillusionment with the worker's paradise. And through the stories of the people--an unhappy young woman who was sold into marriage, Chinas most famous dissident, a doctor who lengthens penises--Wong reveals long-hidden dimensions of the worlds most populous nation.",
+    isbn: '4322301',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/51rPb4-pAQL.jpg'
+  )
+Book.create(
+    title: "Man's Search for Meaning",
+    author: frankyl,
+    genre: Genre.first,
+    description: "Psychiatrist Viktor Frankl's memoir has riveted generations of readers with its descriptions of life in Nazi death camps and its lessons for spiritual survival. Based on his own experience and the stories of his patients, Frankl argues that we cannot avoid suffering but we can choose how to cope with it, find meaning in it, and move forward with renewed purpose. At the heart of his theory, known as logotherapy, is a conviction that the primary human drive is not pleasure but the pursuit of what we find meaningful. Man's Search for Meaning has become one of the most influential books in America; it continues to inspire us all to find significance in the very act of living.",
+    isbn: '7622301',
+    image: 'https://images.penguinrandomhouse.com/cover/9780807067994'
+  )
+Book.create(
+    title: "The Picture of Dorian Gray",
+    author: wilde,
+    genre: Genre.first,
+    description: "Written in his distinctively dazzling manner, Oscar Wilde’s story of a fashionable young man who sells his soul for eternal youth and beauty is the author’s most popular work. The tale of Dorian Gray’s moral disintegration caused a scandal when it ﬁrst appeared in 1890, but though Wilde was attacked for the novel’s corrupting inﬂuence, he responded that there is, in fact, “a terrible moral in Dorian Gray.” Just a few years later, the book and the aesthetic/moral dilemma it presented became issues in the trials occasioned by Wilde’s homosexual liaisons, which resulted in his imprisonment. Of Dorian Gray’s relationship to autobiography, Wilde noted in a letter, “Basil Hallward is what I think I am: Lord Henry what the world thinks me: Dorian what I would like to be—in other ages, perhaps.",
+    isbn: '5922301',
+    image: 'https://www.prestwickhouse.com/ProductImages//ebeb01f5-41da-4f9b-a6cc-763c1610ccff/images/202121.jpg'
+  )
+Book.create!(
+  title: "A Whimsy of the World",
+  author: Author.second,
+  genre: Genre.first,
+  description: "For as long as the town of Aurora could remember, Ellie Watson had been a spirited sort. On her way to the general store to pick up some safety pins for her mother, Ellie could easily take a left instead of a right, and end up on the banks of the Tanner River throwing stones at the silvery shadows of trout. And while getting dressed for the Sunday social, she could suddenly put on her brother’s overalls instead of her new yellow dress – despite her brother being four years older and two inches taller.",
+  isbn: '5990301',
+  image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1558396718l/41952927.jpg'
+  )
+
 
 # BookSubscription
 BookSubscription.create!(book_id: Book.first.id, bookshelf_id: User.first.bookshelves.last.id)
