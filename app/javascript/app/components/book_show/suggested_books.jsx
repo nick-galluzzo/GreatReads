4bdb823ff6697db1f75163989d41a4d2d039a043
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-11 21:52:20 -0500

    initialize

---
+++ app/javascript/app/components/book_show/suggested_books.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import Book from '../book';
+
+class SuggestedBooks extends React.Component {
+
+  renderSuggestedBooks() {
+    const { book, author } = this.props;
+    if (book) {
+      return (
+        book.suggested_books.map((sb) => {
+          return (
+            <div className='suggested-book'>
+              <Book book={sb} author={author} />
+            </div>
+          );
+        })
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className='suggested-books-container'>
+        <div className='suggested-books-title'>
+          <h6>Suggested Books</h6>
+        </div>
+        <div className="suggested-books">
+          { this.renderSuggestedBooks() }
+         </div>
+      </div>
+    );
+  }
+}
+
+export default SuggestedBooks;
