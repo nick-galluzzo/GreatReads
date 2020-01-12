ba4f566e5b47f3d6fc9025a9e136ccd5cdbe5339
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-11 21:50:25 -0500

    fetch author on component update through book selection

--- app/javascript/app/containers/book_show.jsx
+++ app/javascript/app/containers/book_show.jsx
@@ -1,32 +1,42 @@
+// External
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchBook, fetchAuthor } from '../actions/index';
 
-
+// Components
 import AuthorProfile from '../components/book_show/author_profile';
 import BookInfo from '../components/book_show/book_info';
 import SimilarAuthors from '../components/book_show/similar_authors';
 import BookReviews from '../components/book_show/book_reviews';
+import SuggestedBooks from '../components/book_show/suggested_books';
 
 
 class BookShow extends React.Component {
   componentDidMount() {
     this.props.fetchBook(this.props.match.params.id);
+    window.scrollTo(0,0);
   }
 
   componentWillReceiveProps(nextProps) {
+    console.log('mount')
+    console.log(this.props)
     const currentId = this.props.match.params.id;
     const nextId = nextProps.match.params.id;
 
     if (currentId !== nextId) {
       this.props.fetchBook(nextId);
+      window.scrollTo(0,0);
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
+
     if (!this.props.author) {
       this.props.fetchAuthor(this.props.book.author.id);
+    }
+    if (this.props.book && this.props.author && this.props.book.author.id !== this.props.author.id) {
+      this.props.fetchAuthor(this.props.book.author.id)
     }
   }
 
@@ -37,6 +47,7 @@ class BookShow extends React.Component {
           <div className="book-details-container">
             <section className="left-container">
               <BookInfo book={this.props.book} />
+              <SuggestedBooks book={this.props.book} author={this.props.author} />
               <div className="book-reviews">
                 <BookReviews book={this.props.book} />
               </div>
