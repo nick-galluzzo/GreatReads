d77f72702b4726a167d07352cc02b5c72aa8536a
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-12 21:27:44 -0500

    add book reviews

--- app/javascript/app/components/book_show/book_reviews.jsx
+++ app/javascript/app/components/book_show/book_reviews.jsx
@@ -3,25 +3,47 @@ import { Link, withRouter } from 'react-router-dom';
 
 class BookReviews extends React.Component {
 
+  calculateStarRating(rating) {
+    return [...Array(rating)].map((e, i) => <i className="fas fa-star"></i>);
+  }
+
   renderReviews() {
     const { book } = this.props;
-    if (book) {
+    if (book && book.reviews.length > 0) {
       return book.reviews.map((review) => {
         return (
-          <li key={review.id}>
-            <p>{review.comment}</p>
-            <p>({review.user.username})</p>
-          </li>
+          <div className="book-review" key={book.id}>
+            <img src={review.user.image} />
+            <div className="user-info">
+              <div className="rating">
+                <h2>{review.user.username}</h2>
+                <p>{this.calculateStarRating(review.rating)}</p>
+              </div>
+              <div className="comment">
+                <p>{review.comment}</p>
+              </div>
+            </div>
+
+          </div>
         );
       });
     }
+    if (book && book.reviews.length === 0) {
+      return (
+        <p className="empty-review-message">There are no reviews for {book.title} yet... ☹️</p>
+      );
+    }
   }
-
 
   render() {
     return (
-      <div>
-        { this.renderReviews() }
+      <div className='book-reviews-container'>
+        <div className="book-reviews-title">
+          <h6>Community Reviews</h6>
+        </div>
+        <div className="book-reviews">
+          { this.renderReviews() }
+        </div>
       </div>
     );
   }
