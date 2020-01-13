71ac6e782653e688aa80bedbf7d68eed242549a7
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-12 21:30:10 -0500

    add reviewsReducer

--- app/javascript/app/reducers/rootReducer.js
+++ app/javascript/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import booksReducer from './books_reducer';
 import authorsReducer from './authors_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const rootReducer = combineReducers({
   books: booksReducer,
   authors: authorsReducer,
+  reviews: reviewsReducer,
 });
 
 export default rootReducer;
