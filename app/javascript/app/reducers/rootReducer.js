ad526672108a1b712754d4079d1c7489c3deea0a
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-14 15:03:28 -0500

    add a user reducer

--- app/javascript/app/reducers/rootReducer.js
+++ app/javascript/app/reducers/rootReducer.js
@@ -3,13 +3,14 @@ import { reducer as formReducer } from 'redux-form';
 import booksReducer from './books_reducer';
 import authorsReducer from './authors_reducer';
 import reviewsReducer from './reviews_reducer';
-
+import userReducer from './user_reducer';
 
 const rootReducer = combineReducers({
   books: booksReducer,
   authors: authorsReducer,
   reviews: reviewsReducer,
   form: formReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
