c1dd7256d76800c4e42e4311229c1b6a803754e4
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-13 20:07:02 -0500

    made app leading component

--- app/javascript/app/components/app.jsx
+++ app/javascript/app/components/app.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import BookList from '../containers/book_list';
+import BookShow from '../containers/book_show';
+import AuthorShow from '../containers/author_show';
+import ReviewNew from '../containers/review_new';
 
 const App = (props) => {
   return (
     <div className="app">
-      <BookList />
+      <Switch>
+        <Route path="/" exact component={BookList} />
+        <Route exact path="/books/:id" component={BookShow} />
+        <Route path="/authors/:id" exact component={AuthorShow} />
+        <Route path="/reviews/new" exact component={ReviewNew} />
+      </Switch>
     </div>
   );
 };
