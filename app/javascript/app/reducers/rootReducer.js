cd8dc9f7a5a7346d70075c02894854ede5632173
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-13 20:09:22 -0500

    add form reducer

--- app/javascript/app/reducers/rootReducer.js
+++ app/javascript/app/reducers/rootReducer.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import booksReducer from './books_reducer';
 import authorsReducer from './authors_reducer';
 import reviewsReducer from './reviews_reducer';
+
 
 const rootReducer = combineReducers({
   books: booksReducer,
   authors: authorsReducer,
   reviews: reviewsReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
