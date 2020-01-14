4af4b1965abff7967e91356362bfe556a307edfa
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-14 15:04:57 -0500

    initialize

---
+++ app/javascript/app/containers/user_new.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class UserNew extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+        <h1>hi</h1>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default UserNew;
+;
---
+++ app/javascript/app/reducers/user_reducer.js
@@ -0,0 +1,9 @@
+const userReducer = (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
