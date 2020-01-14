270a2ee7564d2dcc915698a70e8cc1daa6cccc22
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-13 20:10:55 -0500

    add reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,18 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get  "books/:id", to: 'pages#home'
   get  "authors/:id", to: 'pages#home'
+  get "reviews/new", to: 'pages#home'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: :show  do
+    resources :users, only: [ :show, :create, :destroy ]  do
       resources :bookshelves, only: [ :index ]
     end
     resources :books, only: [ :index, :show ] do
-      resources :reviews, only: :index
+      resources :reviews, only: [ :index, :create ]
     end
     resources :authors, only: :show
     resources :bookshelves, only: [:show]
     resources :genres, only: :show
-    resources :reviews, only: [:create, :show, :update, :destroy]
+    resources :reviews, only: [:show, :update, :destroy]
   end
 end
