5d17354d82e0d0e6b9395871a9e600e050020219
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-14 18:41:04 -0500

    book subscription has one user through bookshelf

--- app/models/book_subscription.rb
+++ app/models/book_subscription.rb
@@ -1,6 +1,9 @@
 class BookSubscription < ApplicationRecord
-  belongs_to :book
-  belongs_to :bookshelf
+  belongs_to :book, optional: true
+  belongs_to :bookshelf, optional: true
+
+  has_one :user,
+          through: :bookshelf
 
   validates_uniqueness_of :book_id, scope: [:bookshelf_id]
 end
