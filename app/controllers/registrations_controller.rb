d9be7b469deb92e35f407b75edfd7821e2610fc4
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-13 22:03:44 -0500

    parse JSON on user creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,39 @@
 class RegistrationsController < Devise::RegistrationsController
-  def sign_up_params
-    devise_parameter_sanitizer.sanitize(:sign_up)
+  def create
+    @user = User.new(user_params)
+    @user.image = 'https://uploads.scratch.mit.edu/users/avatars/395/5762.png'
+    if @user.save!
+      sign_in(@user)
+      redirect_to root_path
+    else
+      warden.custom_failure!
+      render json: { error: 'signup error' }, status: :unprocessable_entity
+    end
   end
 
-  def account_update_params
-    devise_parameter_sanitizer.sanitize(:account_update)
+  def update
+    @user = User.find_by_email(user_params[:email])
+
+    if @user.update_attributes(user_params)
+      render json: @user
+    else
+      warden.custom_failure!
+      render :json=> @user.errors, :status=>422
+    end
+  end
+
+  def destroy
+    @user = User.find_by_email(user_params[:email])
+    if @user.destroy
+      render :json=> { success: 'user was successfully deleted' }, :status=>201
+    else
+      render :json=> { error: 'user could not be deleted' }, :status=>422
+    end
   end
 
   private
 
-  def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
-
-  def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+  def user_params
+     params.require(:user).permit(:email, :password, :password_confirmation, :username)
   end
 end
