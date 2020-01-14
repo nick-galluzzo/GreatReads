a5aba949a6f050722689d1307e324d0d4820ab47
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-14 15:01:25 -0500

    add initialState carrying user

--- app/javascript/app/components/store.js
+++ app/javascript/app/components/store.js
@@ -3,9 +3,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reduxPromise from 'redux-promise';
 import rootReducer from '../reducers/rootReducer';
 
+const root = document.getElementById('root');
+const initialState = { user: JSON.parse(root.dataset.user) };
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
 
-const store = createStore(rootReducer, {}, middlewares);
+const store = createStore(rootReducer, initialState, middlewares);
 
 export default store;
