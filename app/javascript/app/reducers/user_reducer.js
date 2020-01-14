1bb1a558c0484a27521c6d48ada395ab7b03c0b1
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-14 18:40:34 -0500

    added fetch_user case

--- app/javascript/app/reducers/user_reducer.js
+++ app/javascript/app/reducers/user_reducer.js
@@ -1,6 +1,10 @@
+import { FETCH_USER } from '../actions/index';
+
 const userReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload;
     default:
       return state;
   }
