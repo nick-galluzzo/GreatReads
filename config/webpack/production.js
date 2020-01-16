967ea7254119d030ce54c6f3dc0cc8133a2fba9c
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-15 23:01:47 -0500

    add export

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = {
+  entry: '../../app/javascript/app/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
