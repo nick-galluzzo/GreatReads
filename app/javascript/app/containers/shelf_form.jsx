226b476d56f36e90e67387be82de841ce9d55f02
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-16 21:27:55 -0500

    add shelf form

--- app/javascript/app/containers/shelf_form.jsx
+++ app/javascript/app/containers/shelf_form.jsx
@@ -1,63 +1,97 @@
 import React from 'react';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchShelves, createSubscription } from '../actions/index';
+import { createSubscription, fetchShelves } from '../actions/index';
+// import ShelfButton from '../components/shelf_button';
 
-class ShelfForm extends React.Component {
+const options = [
+  { value: 'want', label: 'Want to Read' },
+  { value: 'read', label: 'Read' },
+  { value: 'current', label: 'Currently Reading' },
+];
 
+class SelectForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inShelf: false,
-      user: false,
+      selectedOption: null,
+      selectedShelfId: null,
+      bookInWantShelf: null,
+      bookInReadShelf: null,
+      bookInCurrentShelf: null
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  renderShelfState() {
-   if (this.props.shelves.length !== 0) {
-      this.props.shelves[0].want.books.map((book) => {
-      if (this.props.book.id === book.id){
-         this.setState({inShelf: true});
+  fetchShelfId(selectedOption) {
+    this.props.shelves.map((shelf) => {
+      if (Object.keys(shelf)[0] === selectedOption.value) {
+        const assignedShelf = Object.keys(shelf)[0]
+        const selectedShelfId = shelf[assignedShelf].id
+        this.setState({selectedShelfId: selectedShelfId})
       }
-    });
-  }
-  }
-
-  renderUserState() {
-    if (this.props.user) {
-      this.setState({user: true});
-    }
+    })
   }
 
-  componentDidMount() {
+  componentWillReceiveProps(nextProps) {
 
 
-    this.props.fetchShelves(this.props.user);
-    this.renderUserState();
-    if (this.props.shelves !== null || this.props.shelves.length !== 0) {
-      this.renderShelfState();
-    }
+  //   if (this.state.selectedOption !== null) {
+  //     console.log('testing..')
+  //     console.log(nextProps.shelves === this.props.shelves)
+  //     nextProps.shelves.map((shelf, id) => {
+  //       if (Object.keys(shelf)[0] === this.state.selectedOption.value) {
+  //         const assignedShelf = Object.keys(shelf)
+  //         const nextShelfCount = shelf[assignedShelf].books.length
+  //         const previousShelfCount = this.props.shelves[id][assignedShelf].books.length
+  //         if (nextShelfCount > previousShelfCount) {
+  //           this.props.fetchShelves(this.props.user)
+  //           // console.log('hi')
+  //         }
+  //       }
+  //       // console.log(Object.values(shelf)[0])
+  //   })
+  // }
+
+
+  //   if (this.props.shelves.length !== 0) {
+  //   console.log(Object.values(nextProps.shelves[this.state.selectedOption]))
+  // }
   }
 
-  // !!!important
-    handleClick() {
-     this.props.createSubscription(this.props.shelves[0].want.id, this.props.book.id);
-     this.props.fetchShelves(this.props.user);
-     this.setState({ inShelf: true });
-    }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.createSubscription(this.state.selectedShelfId, this.props.book.id)
+    this.props.fetchShelves(this.props.user)
+  }
+
+  handleChange (selectedOption) {
+    this.setState({ selectedOption });
+    this.fetchShelfId(selectedOption);
+  };
 
   render() {
-    let btnClass = this.state.inShelf ? 'in-shelf-disabled' : '';
+    const { selectedOption } = this.state;
+    let btnClass = this.state.bookInWantShelf ? 'btn btn-success' : '';
     let disabledClass = this.state.user ? '' : 'hidden';
-    if (this.state.user) {
-      return (
-        <button onClick={() => this.handleClick()} className={[btnClass, disabledClass].join(' ')}>Want To Read</button>
-        )
-    } else {
+
     return (
-      <div></div>
+      <div className='add-to-shelf-container'>
+
+      <form className='add-to-shelf-form' onSubmit={this.handleSubmit}>
+        <fieldset className='form-item'>
+          <legend className="selectedOption">
+              Add:
+              <h1>To do - add form(or buttons for clicking)</h1>
+          </legend>
+        </fieldset>
+        <button className="btn btn-primary">Add To Shelf</button>
+        </form>
+      </div>
     );
-  }
   }
 }
 
@@ -67,7 +101,7 @@ const mapStateToProps = (state) => ({
 });
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchShelves, createSubscription }, dispatch);
+  return bindActionCreators({ createSubscription, fetchShelves }, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShelfForm);
+export default connect(mapStateToProps, mapDispatchToProps)(SelectForm);
