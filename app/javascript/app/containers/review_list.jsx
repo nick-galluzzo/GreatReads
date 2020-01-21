36bb5a8dc7096596f627ffbbee88e9c4e20c0702
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-21 16:43:52 -0500

    remove unnecessary container

--- app/javascript/app/containers/review_list.jsx
+++ app/javascript/app/containers/review_list.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchReviews } from '../actions/index';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import { fetchReviews } from '../actions/index';
 
 
-class ReviewList extends React.Component {
-  renderReviews() {
-    const { books } = this.props;
-    return books.map((book) => <Book book={book} key={book.id} />);
-  }
+// class ReviewList extends React.Component {
+//   renderReviews() {
+//     const { books } = this.props;
+//     return books.map((book) => <Book book={book} key={book.id} />);
+//   }
 
-  render() {
-      return (
-        <div>
-          <h1>hi reviews</h1>
-        </div>
-      );
-  }
-}
+//   render() {
+//       return (
+//         <div>
+//           <h1>hi reviews</h1>
+//         </div>
+//       );
+//   }
+// }
 
-function mapStateToProps(state, ownProps) {
-  return {
-    reviews: state.reviews,
-  };
-}
+// function mapStateToProps(state, ownProps) {
+//   return {
+//     reviews: state.reviews,
+//   };
+// }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchReviews }, dispatch);
-}
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchReviews }, dispatch);
+// }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(ReviewList);
+// export default connect(mapStateToProps, mapDispatchToProps)(ReviewList);
