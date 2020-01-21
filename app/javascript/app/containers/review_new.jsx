0059ab8a7b1c337cc80f46d077fd111726ceb137
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-21 16:35:07 -0500

    added a star rating component to reviews

--- app/javascript/app/containers/review_new.jsx
+++ app/javascript/app/containers/review_new.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { createReview } from '../actions/index';
+import StarRatingComponent from 'react-star-rating-component';
 
-class ReviewNew extends React.Component {
+class EditStarReview extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      rating: '5',
+      rating: 1,
       comment: '',
       book_id: this.props.book.id,
     };
@@ -23,11 +25,6 @@ class ReviewNew extends React.Component {
     );
   }
 
-  update(field) {
-    return (e) => {
-      this.setState({[field]: e.target.value});
-    };
-  }
   renderField(field) {
     return (
       <div className="form-group">
@@ -41,42 +38,48 @@ class ReviewNew extends React.Component {
     );
   }
 
+  update(field) {
+      return (e) => {
+        this.setState({[field]: e.target.value});
+      };
+    }
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({rating: nextValue});
+  }
+
   render() {
+    const { rating } = this.state;
+
     return (
       <div className='main-content-container'>
         <div className="main-content">
           <form className='review_form' onSubmit={this.handleSubmit}>
             <fieldset className='form-item'>
               <legend className="rating">
-                Rating:
-                <select value={this.state.rating}
-                  onChange={this.update('rating')}
-                  >
-                  <option disabled defaultValue value='0'>-</option>
-                  <option value='1'>1</option>
-                  <option value='2'>2</option>
-                  <option value='3'>3</option>
-                  <option value='4'>4</option>
-                  <option value='5'>5</option>
-                </select>
-              </legend>
-            </fieldset>
-
-            <fieldset className='form_item'>
-              <legend> What did you think?</legend>
-              <textarea
-                className='review_comment'
-                rows='12'
-                placeholder='Enter your review'
-                maxLength='500'
-                onChange={this.update('comment')}
-                value={this.state.comment}
-                />
-            </fieldset>
-            <button className="btn btn-dark">Create Review</button>
-          </form>
+        <StarRatingComponent
+          name="rate1"
+          starCount={5}
+          value={rating}
+          onStarClick={this.onStarClick.bind(this)}
+        />
+            </legend>
+          </fieldset>
+          <fieldset className='form_item'>
+            <legend> What did you think?</legend>
+            <textarea
+              className='review_comment'
+              rows='12'
+              placeholder='Enter your review'
+              maxLength='500'
+              onChange={this.update('comment')}
+              value={this.state.comment}
+              />
+          </fieldset>
+              <button className="btn btn-dark">Create Review</button>
+            </form>
+          </div>
         </div>
-      </div>
     );
   }
 }
@@ -88,5 +91,6 @@ const mapStateToProps = (state) => {
 }
 
 export default reduxForm({ form: 'newReviewForm' })(
-  connect(mapStateToProps, { createReview })(ReviewNew),
+  connect(mapStateToProps, { createReview })(EditStarReview),
 );
+
