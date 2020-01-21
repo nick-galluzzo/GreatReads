a92bf3f51a91d1472d460556c54301f39506891f
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-21 14:03:40 -0500

    add structure and formatting to author show page

--- app/javascript/app/containers/author_show.jsx
+++ app/javascript/app/containers/author_show.jsx
@@ -4,10 +4,10 @@ import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import { fetchAuthor } from '../actions/index';
 
-
 import Book from '../components/book';
 
 class AuthorShow extends React.Component {
+
   componentDidMount() {
     if (!this.props.author) {
       this.props.fetchAuthor(this.props.match.params.id);
@@ -27,9 +27,16 @@ class AuthorShow extends React.Component {
     const { author } = this.props;
     if (author) {
       return (
-        <div className="author-information">
-          <h1>{author.name}</h1>
-          <p>Top Genre: {author.top_genre.name}</p>
+        <div className="author-info">
+          <div className="author-image">
+            <img src={author.image} alt=""/>
+          </div>
+          <div className="author-description">
+            <h1>{author.name}</h1>
+            <p id='top-genre'>Top Genre: {author.top_genre.name}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt provident autem ea fugiat natus debitis deserunt deleniti molestias, perspiciatis excepturi consectetur culpa voluptatibus laboriosam! Iure quia aperiam asperiores provident delectus quidem. Veritatis magnam dolorem ipsa explicabo dolores dolor rerum, expedita et quaerat voluptas nihil deserunt doloribus tenetur vero alias minima sint officia optio inventore, aliquid. Cumque commodi, nesciunt veniam rem ipsam possimus ducimus. Sit quod odit non eveniet corporis nemo ratione asperiores suscipit aspernatur adipisci accusantium tempora nihil, molestiae obcaecati harum quae labore vitae amet consectetur aperiam! Inventore ea, fugiat, sed at doloribus fuga enim officia praesentium velit beatae libero.
+            </p>
+          </div>
         </div>
       );
     }
@@ -58,18 +65,29 @@ class AuthorShow extends React.Component {
     }
   }
 
-
   render() {
+      if (this.props.author) {
       return (
-        <div>
-         { this.renderAuthor() }
-        <div className="author-books-list">
-          { this.renderAuthorBooks() }
-         </div>
-         <h3>Similar Authors:</h3>
-          { this.renderSimilarAuthors() }
+        <div className='main-content-container'>
+          <div className="main-content">
+            { this.renderAuthor() }
+            <div className="extended-author-info">
+           <div className="similar-authors">
+           <h3>Similar Authors:</h3>
+            { this.renderSimilarAuthors() }
+            </div>
+          <div className="author-books-list">
+
+            { this.renderAuthorBooks() }
+           </div>
+            </div>
+          </div>
         </div>
       );
+  }
+  return (
+    <div></div>
+  )
   }
 }
 
