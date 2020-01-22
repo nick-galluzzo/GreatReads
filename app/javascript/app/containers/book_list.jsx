0b031ebc961bd6e7c8ac3cb69d8a3006eca52b95
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-21 20:41:57 -0500

    add banner image to home page

--- app/javascript/app/containers/book_list.jsx
+++ app/javascript/app/containers/book_list.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchBooks, fetchUser, fetchShelves } from '../actions/index';
+import { fetchBooks, fetchShelves } from '../actions/index';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import { Link } from 'react-router-dom';
 
 import Book from '../components/book';
+import UserShelves from '../components/user_shelves';
 
 class BookList extends React.Component {
   componentDidMount() {
@@ -19,10 +22,39 @@ class BookList extends React.Component {
     return books.map((book) => <Book book={book} key={book.id} />);
   }
 
+  renderJumbotron() {
+    if (this.props.user) {
+      return (
+      <Jumbotron className='home-header-user'>
+        <h1>Welcome Back!</h1>
+        <p>
+          What are you reading today?
+        </p>
+        <Link to={`/users/${this.props.user}/bookshelves`}>
+          <button className='btn btn-dark shelf-button'>My Shelves</button>
+        </Link>
+      </Jumbotron>
+      )
+    } else {
+      return (
+      <Jumbotron className='home-header'>
+        <h1>Welcome to GreatReads!</h1>
+        <p>
+          Sign up to discover your next favorite book.
+        </p>
+      </Jumbotron>
+      )
+    }
+  }
+
   render() {
       return (
         <div className="main-content-container">
           <div className="main-content">
+            <div className="user-shelves">
+              { this.renderJumbotron() }
+            </div>
+            <h1 id='book-list-title'><i class="fas fa-book-reader"></i></h1>
            <div className='home-book-list'>
             { this.renderBook() }
            </div>
