f3cbff74d5525be8b0e1303d0ca306f2309fefb3
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-22 17:39:38 -0500

    add usershelves routet

--- app/javascript/app/components/app.jsx
+++ app/javascript/app/components/app.jsx
@@ -6,7 +6,7 @@ import BookShow from '../containers/book_show';
 import AuthorShow from '../containers/author_show';
 import ReviewNew from '../containers/review_new';
 import UserNew from '../containers/user_new';
-
+import UserShelves from '../containers/user_shelves';
 
 const App = (props) => {
   return (
@@ -16,8 +16,7 @@ const App = (props) => {
         <Route exact path="/books/:id" component={BookShow} />
         <Route path="/authors/:id" exact component={AuthorShow} />
         <Route path="/reviews/new" exact component={ReviewNew} />
-
-
+        <Route path="/users/:id/bookshelves" exact component={UserShelves} />
       </Switch>
     </div>
   );
